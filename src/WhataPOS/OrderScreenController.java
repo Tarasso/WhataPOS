b38dc35a57616035a4d5f2a7bfc8148afede50c9
kylelamb1324 <13280+kylelamb1324@users.noreply.github.tamu.edu>
2021-03-07 18:12:02 -0600

    Changes to ordering screen

--- src/WhataPOS/OrderScreenController.java
+++ src/WhataPOS/OrderScreenController.java
@@ -1,22 +1,153 @@
 package WhataPOS;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.net.URL;
+import java.sql.Array;
 
-public class OrderScreenController {
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+public class OrderScreenController implements Initializable {
 
     // Element tag ids
-    @FXML private ListView menuListView;
+    @FXML private TableView menuTableView;
     @FXML private ListView orderListView;
+    @FXML private TextArea orderTextArea;
+    @FXML private TableView orderTableView;
+    private float orderTotal = 0;
+    private final double TAXPERCENT = 1.0825;
+
+
+
+
+    // Gathers data for beverages from database
+    public ObservableList<Beverage> getBeverages() {
+        ObservableList<Beverage> beverages = FXCollections.observableArrayList();
+        try {
+            String sql = "select * from beverages";
+            ResultSet rs = JDBC.execQuery(sql);
+            while (rs.next()) {
+                beverages.add(new Beverage(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getInt("availableQuantity"),
+                        rs.getDouble("costToMake"),
+                        rs.getDouble("salePrice")
+                ));
+            }
+        } catch(SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        }
+        return beverages;
+    }
+
+    // Gathers data for Entrees for database
+    public ObservableList<Entree> getEntrees() {
+        ObservableList<Entree> entrees = FXCollections.observableArrayList();
+        try {
+            String sql = "select * from entrees";
+            ResultSet rs = JDBC.execQuery(sql);
+            while (rs.next()) {
+                entrees.add(new Entree(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getString("type"),
+                        rs.getInt("availableQuantity"),
+                        rs.getDouble("costToMake"),
+                        rs.getDouble("salePrice"),
+                        rs.getArray("toppings")
+                ));
+            }
+        } catch(SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        }
+        return entrees;
+    }
+
+    // Gathers data for desserts from database
+    public ObservableList<Dessert> getDesserts() {
+        ObservableList<Dessert> desserts = FXCollections.observableArrayList();
+        try {
+            String sql = "select * from desserts";
+            ResultSet rs = JDBC.execQuery(sql);
+            while (rs.next()) {
+                desserts.add(new Dessert(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getInt("availableQuantity"),
+                        rs.getDouble("costToMake"),
+                        rs.getDouble("salePrice")
+                ));
+            }
+        } catch(SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        }
+        return desserts;
+    }
+
+
+    // Gathers data for toppings from database
+    public ObservableList<Topping> getToppings() {
+        ObservableList<Topping> toppings = FXCollections.observableArrayList();
+        try {
+            String sql = "select * from toppings";
+            ResultSet rs = JDBC.execQuery(sql);
+            while (rs.next()) {
+                toppings.add(new Topping(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getInt("availableQuantity"),
+                        rs.getDouble("costToMake"),
+                        rs.getDouble("salePrice")
+                ));
+            }
+        } catch(SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        }
+        return toppings;
+    }
+
+    // Gathers data for sides from database
+    public ObservableList<Side> getSides() {
+        ObservableList<Side> sides = FXCollections.observableArrayList();
+        try {
+            String sql = "select * from sides";
+            ResultSet rs = JDBC.execQuery(sql);
+            while (rs.next()) {
+                sides.add(new Side(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        rs.getInt("availableQuantity"),
+                        rs.getDouble("costToMake"),
+                        rs.getDouble("salePrice")
+                ));
+            }
+        } catch(SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        }
+        return sides;
+    }
 
     // Buttons
     @FXML
@@ -31,44 +162,285 @@ public class OrderScreenController {
 
     @FXML
     public void actionShowEntrees(ActionEvent event) throws IOException {
-        menuListView.getItems().clear();
-        menuListView.getItems().addAll("Whataburger 1", "Whataburger 2", "Whataburger 3");
-        menuListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        TableColumn<Entree, String> nameColumn = new TableColumn<>("name");
+        nameColumn.setMinWidth(20);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Entree, String> priceColumn = new TableColumn<>("salePrice");
+        priceColumn.setMinWidth(20);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("salePrice"));
+
+        menuTableView.getColumns().clear();
+        menuTableView.setItems(getEntrees());
+        menuTableView.getColumns().addAll(nameColumn, priceColumn);
+
     }
 
     @FXML
     public void actionShowBeverages(ActionEvent event) throws IOException {
-        menuListView.getItems().clear();
-        menuListView.getItems().addAll("Beverage 1", "Beverage 2", "Beverage 3");
-        menuListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        TableColumn<Beverage, String> nameColumn = new TableColumn<>("name");
+        nameColumn.setMinWidth(20);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Beverage, String> priceColumn = new TableColumn<>("salePrice");
+        priceColumn.setMinWidth(20);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("salePrice"));
+
+        menuTableView.getColumns().clear();
+        menuTableView.setItems(getBeverages());
+        menuTableView.getColumns().addAll(nameColumn, priceColumn);
     }
 
     @FXML
     public void actionShowDesserts(ActionEvent event) throws IOException {
-        menuListView.getItems().clear();
-        menuListView.getItems().addAll("Dessert 1", "Dessert 2", "Dessert 3");
-        menuListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        TableColumn<Dessert, String> nameColumn = new TableColumn<>("name");
+        nameColumn.setMinWidth(20);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Dessert, String> priceColumn = new TableColumn<>("salePrice");
+        priceColumn.setMinWidth(20);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("salePrice"));
+
+        menuTableView.getColumns().clear();
+        menuTableView.setItems(getDesserts());
+        menuTableView.getColumns().addAll(nameColumn, priceColumn);
     }
 
     @FXML
     public void actionShowSides(ActionEvent event) throws IOException {
-        menuListView.getItems().clear();
-        menuListView.getItems().addAll("Side 1", "Side 2", "Side 3");
-        menuListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        TableColumn<Side, String> nameColumn = new TableColumn<>("name");
+        nameColumn.setMinWidth(20);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Side, String> priceColumn = new TableColumn<>("salePrice");
+        priceColumn.setMinWidth(20);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("salePrice"));
+
+        menuTableView.getColumns().clear();
+        menuTableView.setItems(getSides());
+        menuTableView.getColumns().addAll(nameColumn, priceColumn);
     }
 
     @FXML
     public void actionShowToppings(ActionEvent event) throws IOException {
-        menuListView.getItems().clear();
-        menuListView.getItems().addAll("Topping 1", "Topping 2", "Topping 3");
-        menuListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
     }
 
     @FXML
     public void actionSelectItem(ActionEvent event) throws IOException {
-        ObservableList listOfItems = menuListView.getSelectionModel().getSelectedItems();
-        orderListView.getItems().addAll(listOfItems);
+        var object = menuTableView.getSelectionModel().getSelectedItem();
+
+
+        double totalAfterTax = 0;
+        /*
+        switch (object.getClass().getName()) {
+            case "WhataPOS.Beverage":
+                Beverage selectedBeverage = (Beverage) object;
+                orderListView.getItems().add(selectedBeverage.getName());
+
+                orderTotal += selectedBeverage.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+                break;
+
+            case "WhataPOS.Entree":
+                Entree selectedEntree = (Entree) object;
+                orderListView.getItems().add(selectedEntree.getName());
+
+                orderTotal += selectedEntree.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+                break;
+
+            case "WhataPOS.Side":
+                Side selectedSide = (Side) object;
+                orderListView.getItems().add(selectedSide.getName());
+
+                orderTotal += selectedSide.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+                break;
+
+            case "WhataPOS.Dessert":
+                Dessert selectedDessert = (Dessert) object;
+                orderListView.getItems().add(selectedDessert.getName());
+
+                orderTotal += selectedDessert.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                        + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+                break;
+        }
+        */
+        switch (object.getClass().getName()) {
+            case "WhataPOS.Beverage":
+                Beverage selectedBeverage = (Beverage) object;
+                orderTableView.getItems().add(selectedBeverage);
+
+                orderTotal += selectedBeverage.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+                break;
+
+            case "WhataPOS.Entree":
+                Entree selectedEntree = (Entree) object;
+
+                orderTableView.getItems().add(selectedEntree);
+
+                orderTotal += selectedEntree.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+                break;
+
+            case "WhataPOS.Side":
+                Side selectedSide = (Side) object;
+                orderTableView.getItems().add(selectedSide);
+
+                orderTotal += selectedSide.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+                break;
+
+            case "WhataPOS.Dessert":
+                Dessert selectedDessert = (Dessert) object;
+                orderTableView.getItems().add(selectedDessert);
+
+                orderTotal += selectedDessert.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+                break;
+        }
+
     }
 
+    public void actionDeleteItem(ActionEvent event) throws IOException{
+        var object = orderTableView.getSelectionModel().getSelectedItem();
+
+
+        double totalAfterTax = 0;
+
+        switch (object.getClass().getName()) {
+            case "WhataPOS.Beverage":
+                Beverage selectedBeverage = (Beverage) object;
+
+                orderTableView.getItems().remove(selectedBeverage);
+                orderTotal -= selectedBeverage.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+                break;
+
+            case "WhataPOS.Entree":
+                Entree selectedEntree = (Entree) object;
+
+                orderTableView.getItems().remove(selectedEntree);
+                orderTotal -= selectedEntree.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+
+                break;
+
+            case "WhataPOS.Side":
+                Side selectedSide = (Side) object;
+
+                orderTableView.getItems().remove(selectedSide);
+                orderTotal -= selectedSide.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+
+                break;
+
+            case "WhataPOS.Dessert":
+                Dessert selectedDessert = (Dessert) object;
+
+                orderTableView.getItems().remove(selectedDessert);
+                orderTotal -= selectedDessert.getSalePrice();
+                totalAfterTax = orderTotal * TAXPERCENT;
+
+                orderTextArea.setText(
+                        "Order Total: " + String.format("%.2f", orderTotal) + "\n"
+                                + "Total After Tax: " + String.format("%.2f", totalAfterTax)
+                );
+
+
+                break;
+        }
+
+
+
+
+
+    }
+
+    @Override
+    public void initialize (URL url, ResourceBundle resourceBundle) {
+        orderTextArea.setEditable(false);
+
+        TableColumn<Side, String> nameColumn = new TableColumn<>("name");
+        nameColumn.setMinWidth(20);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+
+        TableColumn<Side, String> priceColumn = new TableColumn<>("salePrice");
+        priceColumn.setMinWidth(20);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("salePrice"));
+
+        orderTableView.getColumns().addAll(nameColumn, priceColumn);
+    }
 
 }
